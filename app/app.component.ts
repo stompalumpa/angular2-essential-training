37d87b9b0b44bf55e9cf41b3facfa9fb0af5d5e7
bernieboy <bereir15@student.westerdals.no>
2016-12-17 01:57:22 +0100

    edtCSS

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  template: '<h1>My App</h1>'
+  selector: 'mw-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
 
