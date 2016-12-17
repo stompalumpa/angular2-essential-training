aa4ed1d8e910f9591c31d165a1ea5a2545e9e8c0
bernieboy <bereir15@student.westerdals.no>
2016-12-17 01:08:10 +0100

    ex1

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppModule
+  ],
+  bootstrap: [
+    AppModule
+  ]
+})
+export class AppModule {}
