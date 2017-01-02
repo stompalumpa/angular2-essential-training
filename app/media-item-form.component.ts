87556b775875ca7c9871e12797bc53c3187b08cf
bernieboy <bereir15@student.westerdals.no>
2017-01-02 05:46:27 +0100

    just following tutorials

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -6,6 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+  form;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
+    });
+  }
+
+  yearValidator(control) {
+    if(control.value.trim().length === 0) {
+      return null;
+    }
+    let year = parseInt(control.value);
+    let minYear = 1800;
+    let maxYear = 2500;
+    if(year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { 'year': {
+        min: minYear,
+        max: maxYear
+      } };
+    }
+  }
+
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }
