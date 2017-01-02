3efd6b258e8e38fb7aee1b224a1bb8e861c2a3f0
bernieboy <bereir15@student.westerdals.no>
2017-01-02 18:33:27 +0100

    following tutorials

--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -8,16 +8,30 @@ import { MediaItemService } from './media-item.service';
   styleUrls: ['app/media-item-list.component.css']
 })
 export class MediaItemListComponent {
-  mediaItems;
+  medium = '';
+  mediaItems = [];
 
   constructor(private mediaItemService: MediaItemService) {}
 
   ngOnInit() {
-    this.mediaItems = this.mediaItemService.get();
+    this.getMediaItems(this.medium);
   }
 
   onMediaItemDelete(mediaItem) {
-    this.mediaItemService.delete(mediaItem);
+    this.mediaItemService.delete(mediaItem)
+      .subscribe(() => {
+        this.getMediaItems(this.medium);
+      });
+  }
+
+
+
+  getMediaItems(medium) {
+    this.medium = medium;
+    this.mediaItemService.get(medium)
+    .subscribe(mediaItems => {
+      this.mediaItems = mediaItems;
+    })
   }
 
 }
